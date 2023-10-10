0a94c233e535e0b44f4d8138735b75f4a22dccf2
Marin2705 <40020721+Marin2705@users.noreply.github.com>
2023-10-10 11:29:14 +0200

    add interaction and coverage tests on Page

--- src/stories/Page.stories.js
+++ src/stories/Page.stories.js
@@ -1,25 +1,32 @@
-import { within, userEvent } from '@storybook/testing-library';
+import { within, userEvent } from "@storybook/testing-library";
 
-import { Page } from './Page';
+import { Page } from "./Page";
 
 export default {
-  title: 'Example/Page',
+  title: "Example/Page",
   component: Page,
   parameters: {
     // More on how to position stories at: https://storybook.js.org/docs/react/configure/story-layout
-    layout: 'fullscreen',
+    layout: "fullscreen",
+  },
+  play: async ({ canvasElement }) => {
+    const canvas = within(canvasElement);
+    const signupButton = await canvas.getByRole("button", {
+      name: /Sign up/i,
+    });
+    await userEvent.click(signupButton);
+    const logoutButton = await canvas.getByRole("button", {
+      name: /Log out/i,
+    });
+    await userEvent.click(logoutButton);
+    const loginButton = await canvas.getByRole("button", {
+      name: /Log in/i,
+    });
+    await userEvent.click(loginButton);
   },
 };
 
 export const LoggedOut = {};
 
 // More on interaction testing: https://storybook.js.org/docs/react/writing-tests/interaction-testing
-export const LoggedIn = {
-  play: async ({ canvasElement }) => {
-    const canvas = within(canvasElement);
-    const loginButton = await canvas.getByRole('button', {
-      name: /Log in/i,
-    });
-    await userEvent.click(loginButton);
-  },
-};
+export const LoggedIn = {};
